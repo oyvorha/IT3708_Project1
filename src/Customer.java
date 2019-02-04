03437c5998323cfa7a218ac33318c9b53bc67f62
oyvorsh <oyvorsh@stud.ntnu.no>
2019-02-04 09:48:10 +0100

    Add coordinate to customer and check for visited

--- src/Customer.java
+++ src/Customer.java
@@ -1,12 +1,16 @@
+import java.util.Objects;
+
 public class Customer {
 
     private int customerNo;
     private int demand;
     private Vehicle visitingVehicle;
+    private Coordinate coordinate;
 
-    Customer(int customer_no, int demand) {
+    Customer(int customer_no, int demand, Coordinate coordinate) {
         this.customerNo = customer_no;
         this.demand = demand;
+        this.coordinate = coordinate;
     }
 
     public int getCustomerNo() {
@@ -17,7 +21,16 @@ public class Customer {
         return demand;
     }
 
+    public Coordinate getCoordinate() {
+        return coordinate;
+    }
+
     public void setVisitingVehicle(Vehicle visitingVehicle) {
         this.visitingVehicle = visitingVehicle;
     }
+
+    public boolean visited() {
+        return !Objects.isNull(this.visitingVehicle);
+    }
+
 }
