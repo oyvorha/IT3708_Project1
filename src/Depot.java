9630f5ee4e7c2a850d81c2b501be3b6a152239a9
oyvorsh <oyvorsh@stud.ntnu.no>
2019-02-04 14:12:27 +0100

    update depot class

--- src/Depot.java
+++ src/Depot.java
@@ -1,16 +1,36 @@
 public class Depot {
 
-    private Coordinate cor;
+    private Coordinate coordinate;
     private int depotID;
     private int maxDuration;
     private int maxLoad;
-    private int maxVehiclesAvailable;
+    private int maxStartingVehiclesAvailable;
 
-    Depot(int depotID, int maxDuration, int maxLoad, int maxVehiclesAvailable) {
+    Depot(int depotID, int maxDuration, int maxLoad, int maxVehiclesAvailable, Coordinate coordinate) {
         this.depotID = depotID;
         this.maxDuration = maxDuration;
         this.maxLoad = maxLoad;
-        this.maxVehiclesAvailable = maxVehiclesAvailable;
+        this.maxStartingVehiclesAvailable= maxVehiclesAvailable;
+        this.coordinate = coordinate;
     }
 
+    public Coordinate getCoordinate() {
+        return coordinate;
+    }
+
+    public int getDepotID() {
+        return depotID;
+    }
+
+    public int getMaxDuration() {
+        return maxDuration;
+    }
+
+    public int getMaxLoad() {
+        return maxLoad;
+    }
+
+    public int getMaxStartingVehiclesAvailable() {
+        return maxStartingVehiclesAvailable;
+    }
 }
