65eee6c8fbfd07a63b33b1bdb8f7b543e2beaf82
oyvorsh <oyvorsh@stud.ntnu.no>
2019-02-04 14:49:03 +0100

    Add chromosome class

---
+++ src/Chromosome.java
@@ -0,0 +1,14 @@
+import java.util.ArrayList;
+
+public class Chromosome {
+
+    private ArrayList<Depot> depots;
+
+    public Chromosome() {
+        this.depots = new ArrayList<>();
+    }
+
+    public ArrayList<Depot> getDepots() {
+        return depots;
+    }
+}
