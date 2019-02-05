799847cd4b1497a65e1f820b1b784703807cb081
Sigrid Fosen <sigrif@stud.ntnu.no>
2019-02-05 15:03:25 +0100

    Modify Vehicle class to enable Route object

--- src/Vehicle.java
+++ src/Vehicle.java
@@ -1,65 +1,26 @@
-import java.util.ArrayList;
 
 public class Vehicle {
 
-    private int currentLoad;
+    private int maxLoad;
     private int vehicleID;
-    private Depot startDepot;
-    private Depot endDepot;
-    private ArrayList<Customer> customers;
+    private int maxDuration;
 
-    Vehicle(int vehicleID) {
-        this.currentLoad = 0;
+
+    Vehicle(int vehicleID, int maxLoad, int maxDuration) {
+        this.maxLoad = maxLoad;
         this.vehicleID = vehicleID;
-        this.customers = new ArrayList<>();
+        this.maxDuration = maxDuration;
     }
 
-    public void addCustomer(Customer customer) {
-        if (!customer.visited() && !customers.contains(customer)) {
-            customers.add(customer);
-            customer.setVisitingVehicle(this);
-        }
-    }
-
-    public int calculateRoute() {
-        int distance = 0;
-        Coordinate previousCoordinate = startDepot.getCoordinate();
-        for (Customer c : customers) {
-            distance += c.getCoordinate().getEuclidianDistance(previousCoordinate);
-            previousCoordinate = c.getCoordinate();
-        }
-        return distance;
-    }
-
-    public void setCurrentLoad(int currentLoad) {
-        this.currentLoad = currentLoad;
-    }
-
-    public void setStartDepot(Depot startDepot) {
-        this.startDepot = startDepot;
-    }
-
-    public void setEndDepot(Depot endDepot) {
-        this.endDepot = endDepot;
-    }
-
-    public Depot getEndDepot() {
-        return endDepot;
-    }
-
-    public Depot getStartDepot() {
-        return startDepot;
-    }
-
-    public int getCurrentLoad() {
-        return currentLoad;
+    @Override
+    public String toString() {
+        return "Vehicle "+this.getVehicleID();
     }
 
     public int getVehicleID() {
         return vehicleID;
     }
 
-    public ArrayList<Customer> getCustomers() {
-        return customers;
-    }
+    public int getMaxLoad() { return maxLoad; }
+
 }
