d76f0aa9a6bbd84595f7ff7595f83c22699acf53
Sigrid Fosen <sigrif@stud.ntnu.no>
2019-02-05 15:03:03 +0100

    Modify readfrom file to adapt to class changes

--- src/ReadFromFile.java
+++ src/ReadFromFile.java
@@ -8,16 +8,34 @@ import java.util.Arrays;
 
 public class ReadFromFile {
 
-    private ArrayList<Depot> depotList = new ArrayList<>();
-    private ArrayList<Customer> custList = new ArrayList<>();
+    private ArrayList<Depot> depots;
+    private ArrayList<Customer> customers;
+    private ArrayList<Vehicle> vehicles;
 
+    private String filepath;
+    private int depotCount;
+    private int vehicleCount;
+    private int depotCountForCoordinate;
+    private int vehiclesPerDepot;
 
-    public ArrayList<List<Integer>> fileToIntArray(String filepath){
+    public ReadFromFile(String filepath){
+        this.depotCount = 0;
+        this.vehicleCount = 1;
+        this.depotCountForCoordinate = 0;
+        this.vehiclesPerDepot = 0;
+        this.filepath = filepath;
+        this.depots = new ArrayList<>();
+        this.customers = new ArrayList<>();
+        this.vehicles= new ArrayList<>();
+        this.initialize(this.fileToIntArray());
+    }
+
+    public ArrayList<List<Integer>> fileToIntArray(){
         String strLine = "";
         ArrayList<List<String>> fileArray = new ArrayList<>();
         ArrayList<List<Integer>> intArray = new ArrayList<>();
         try {
-            BufferedReader br = new BufferedReader(new FileReader(filepath));
+            BufferedReader br = new BufferedReader(new FileReader(this.filepath));
             while (strLine != null) {
                 strLine = br.readLine();
                 if (strLine == null) {
@@ -43,65 +61,64 @@ public class ReadFromFile {
     }
 
     public void initialize(ArrayList<List<Integer>> intArray){
-        int depCount = 0;
-        int custCount = 0;
-        int carsPerDepot = 0;
+
         for (int i=0; i<intArray.size(); i++){
             List<Integer> intList = intArray.get(i);
             if (intList.size() == 3){
-                //General depot info, 1 line
-                carsPerDepot = intList.get(0);
+                //General depot info, one line
+                this.vehiclesPerDepot = intList.get(0);
             }
             else if (intList.size() == 2){
                 //Specs for each 1...n depot
-                //initialize depot and add to depotList
-                Depot d = new Depot(depCount, intList.get(0), intList.get(1),
-                        carsPerDepot, new Coordinate(0,0));
-                depotList.add(d);
+                //initialize depot and add to depots
+                Depot d = new Depot(this.depotCount+1, intList.get(0), intList.get(1),
+                        this.vehiclesPerDepot, new Coordinate(0,0));
+                this.depots.add(d);
+                this.depotCount ++;
+                //initialize vehiclesPerDepot number of vehicles per depot
+                for (int j = 0; j<this.vehiclesPerDepot; j++){
+                    this.vehicles.add(new Vehicle(this.vehicleCount, intList.get(0), intList.get(1)));
+                    this.vehicleCount++;
+                }
             }
             else if (intList.size() == 7){
                 //Depot coordinates for each 1...n depot
-                Depot d = depotList.get(depCount);
+                Depot d = this.depots.get(this.depotCountForCoordinate);
                 Coordinate dc = new Coordinate(intList.get(1), intList.get(2));
                 d.setCoordinate(dc);
-                depCount ++;
+                this.depotCountForCoordinate++;
             }
             else {
-                //Specs for each 1...m customer. Initialize customer per line
-                //initialize customers and add to custList
+                //Specs for each 1...m customer. Initialize one customer per line
                 Coordinate cc = new Coordinate(intList.get(1), intList.get(2));
-                int service_dur = intList.get(3);
+                int serviceDuration = intList.get(3);
                 int demand = intList.get(4);
-                Customer c = new Customer(custCount, service_dur, demand, cc);
-                custList.add(c);
-                custCount++;
+                Customer c = new Customer(intList.get(0), serviceDuration, demand, cc);
+                this.customers.add(c);
             }
         }
     }
-    /*
-    public void getFirstSolution(){
-        int vehicleCount = 0;
-        for each depot in depot list:
-            instansier max_no_vehicles antall Vehicles and add as vehicles(field)
+
+    public ArrayList<Depot> getDepots(){
+        return this.depots;
     }
-    */
+
+    public ArrayList<Customer> getCustomers(){
+        return this.customers;
+    }
+
+    public ArrayList<Vehicle> getVehicles(){
+        return this.vehicles;
+    }
+
+    public int getVehiclesPerDepot(){
+        return this.vehiclesPerDepot;
+    }
 
     public static void main(String[] args) {
         String filepath = "./Files/DataFiles/p01";
-        ReadFromFile rff = new ReadFromFile();
-        /*
-        for (int i=0; i<rff.fileToIntArray(filepath).size(); i++){
-            System.out.println(rff.fileToIntArray(filepath).get(i));
-        }
-        */
-        rff.initialize(rff.fileToIntArray(filepath));
-        System.out.println("No of customers: "+rff.custList.size());
-        System.out.println("No of depots: "+rff.depotList.size());
-        for (int i = 0; i<rff.custList.size(); i++){
-            System.out.println(rff.custList.get(0));
-        }
-        for (int i = 0; i<rff.depotList.size(); i++){
-            System.out.println(rff.depotList.get(0));
-        }
+        ReadFromFile readFromFile = new ReadFromFile(filepath);
+        System.out.println("No of customers: "+readFromFile.customers.size());
+        System.out.println("No of depots: "+readFromFile.depots.size());
     }
 }
