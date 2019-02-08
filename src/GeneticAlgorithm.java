7f65bb09dfd35c42a86fb258e2f6798e45adf0f9
oyvorsh <oyvorsh@stud.ntnu.no>
2019-02-08 14:41:03 +0100

    Add mutation type 1: change order of customer in route

--- src/GeneticAlgorithm.java
+++ src/GeneticAlgorithm.java
@@ -1,5 +1,6 @@
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Random;
 
 public class GeneticAlgorithm {
 
@@ -7,6 +8,7 @@ public class GeneticAlgorithm {
     private double crossoverRate;
     private int populationSize;
     private ArrayList<Chromosome> rankedChromosomes;
+    private Random random;
 
     public GeneticAlgorithm(double mutationRate, double crossoverRate, int populationSize, ArrayList<Chromosome> chromosomes) {
         this.mutationRate = mutationRate;
@@ -14,11 +16,15 @@ public class GeneticAlgorithm {
         this.populationSize = populationSize;
         this.rankedChromosomes = chromosomes;
         this.rankChromosomes();
+        this.random = new Random();
     }
 
     private void rankChromosomes() {
         System.out.println("Unsorted chromosomes: "+this.rankedChromosomes);
         Collections.sort(this.rankedChromosomes);
+        if (this.rankedChromosomes.size() > populationSize){
+            this.rankedChromosomes.subList(0, populationSize);
+        }
     }
 
     public ArrayList<Chromosome> getRankedChromosomes() {
@@ -26,14 +32,21 @@ public class GeneticAlgorithm {
     }
 
     public void crossover() {
-
+        System.out.println("Do crossover");
     }
 
-    public void mutation(){
-
+    public void mutation1(){
+        for (Chromosome c : this.getRankedChromosomes()){
+            int doMutation = this.random.nextInt(100);
+            if (doMutation < mutationRate){
+                int routeNumber = this.random.nextInt(c.getRoutes().size()-1);
+                Route mutationRoute = c.getRoutes().get(routeNumber);
+                if (mutationRoute.getNodes().size() > 4) {
+                        int index1 = this.random.nextInt(mutationRoute.getNodes().size()-3)+1;
+                        int index2 = this.random.nextInt(mutationRoute.getNodes().size()-3)+1;
+                        mutationRoute.swapCustomers(index1, index2);
+                    }
+                }
+            }
+        }
     }
-
-
-
-
-}
